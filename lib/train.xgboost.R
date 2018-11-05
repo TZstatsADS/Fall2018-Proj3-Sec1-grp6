6c6e34f41c76d3f71c7e3f6a7068c7d2dcfe289c
paigejin <37782326+paigejin@users.noreply.github.com>
2018-11-04 19:53:11 -0500

    Add files via upload

---
+++ lib/train.xgboost.R
@@ -0,0 +1,52 @@
+#########################################################
+### Train a classification model with training features ###
+#########################################################
+
+### Author: Chengliang Tang
+### Project 3
+
+
+train_xgboost <- function(dat_train, label_train, par=NULL){
+  
+  ### Train a Gradient Boosting Model (GBM) using processed features from training images
+  
+  ### Input: 
+  ###  -  features from LR images 
+  ###  -  responses from HR images
+  ### Output: a list for trained models
+  
+  ### load libraries
+  library("xgboost")
+  
+  ### creat model list
+  modelList <- list()
+  
+  ### Train with gradient boosting model
+  if(is.null(par)){
+    max_depth <- 2
+    eta <- 1
+  } else {
+    max_depth <- par[1]
+    eta <- par[2]
+  }
+  
+  ### the dimension of response arrat is * x 4 x 3, which requires 12 classifiers
+  ### this part can be parallelized
+  for (i in 1:12){
+    ## calculate column and channel
+    c1 <- (i-1) %% 4 + 1
+    c2 <- (i-c1) %/% 4 + 1
+    featMat <- dat_train[, , c2]
+    labMat <- label_train[, c1, c2]
+    fit_xgboost <- xgboost(data=featMat, label=labMat,
+                       max_depth=max_depth,
+                       eta=eta,
+                       nthread=2,
+                       nrounds=2,
+                       objective="binary:logistic", 
+                       verbose=FALSE)
+    modelList[[i]] <- list(fit=fit_xgboost)
+  }
+  
+  return(modelList)
+}
